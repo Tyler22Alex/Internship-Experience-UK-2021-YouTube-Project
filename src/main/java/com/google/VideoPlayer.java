6ba5161c8ed21ce7de9533c12d9c98ad245a8e9f
Tyler Dalke <TylerADalke22@hotmail.com>
2021-08-08 14:19:53 -0500

    Finished part 2.

--- src/main/java/com/google/VideoPlayer.java
+++ src/main/java/com/google/VideoPlayer.java
@@ -12,12 +12,14 @@ public class VideoPlayer {
   private Video currentVideo;
   private boolean paused;
 
+  private List<VideoPlaylist> playlists = new ArrayList<>();
+
   public VideoPlayer() {
     this.videoLibrary = new VideoLibrary();
   }
 
   public void numberOfVideos() {
-    System.out.printf("%s videos in the library%n", videoLibrary.getVideos().size());
+    System.out.printf("%s videos in the library\n", videoLibrary.getVideos().size());
   }
 
   public void showAllVideos() {
@@ -39,10 +41,10 @@ public class VideoPlayer {
       Video video = videoLibrary.getVideo(videoId);
       String title = video.getTitle();
       if (currentVideo == null) {
-        System.out.printf("Playing video: %s%n", title);
+        System.out.printf("Playing video: %s\n", title);
       } else {
-        System.out.printf("Stopping video: %s%n", currentVideo.getTitle());
-        System.out.printf("Playing video: %s%n", title);
+        System.out.printf("Stopping video: %s\n", currentVideo.getTitle());
+        System.out.printf("Playing video: %s\n", title);
       }
       currentVideo = video;
       paused = false;
@@ -59,7 +61,7 @@ public class VideoPlayer {
     }
     else
     {
-      System.out.printf("Stopping video: %s%n", currentVideo.getTitle());
+      System.out.printf("Stopping video: %s\n", currentVideo.getTitle());
       currentVideo = null;
     }
   }
@@ -81,11 +83,11 @@ public class VideoPlayer {
     }
     else if (paused)
     {
-      System.out.printf("Video already paused: %s%n", currentVideo.getTitle());
+      System.out.printf("Video already paused: %s\n", currentVideo.getTitle());
     }
     else
     {
-      System.out.printf("Pausing video: %s%n", currentVideo.getTitle());
+      System.out.printf("Pausing video: %s\n", currentVideo.getTitle());
       paused = true;
     }
   }
@@ -101,7 +103,7 @@ public class VideoPlayer {
     }
     else
     {
-      System.out.printf("Continuing video: %s%n", currentVideo.getTitle());
+      System.out.printf("Continuing video: %s\n", currentVideo.getTitle());
       paused = false;
     }
   }
@@ -113,40 +115,153 @@ public class VideoPlayer {
     }
     else if (paused)
     {
-      System.out.printf("Currently playing: %s - PAUSED%n", currentVideo.toString());
+      System.out.printf("Currently playing: %s - PAUSED\n", currentVideo.toString());
     }
     else
     {
-      System.out.printf("Currently playing: %s%n", currentVideo.toString());
+      System.out.printf("Currently playing: %s\n", currentVideo.toString());
     }
   }
 
   public void createPlaylist(String playlistName) {
-    System.out.println("createPlaylist needs implementation");
+    for (VideoPlaylist playlist : playlists) {
+      if (playlistName.equalsIgnoreCase(playlist.getName())) {
+        System.out.println("Cannot create playlist: A playlist with the same name already exists");
+        return;
+      }
+    }
+
+    VideoPlaylist playlist = new VideoPlaylist(playlistName);
+    playlists.add(playlist);
+
+    System.out.printf("Successfully created new playlist: %s\n", playlistName);
   }
 
   public void addVideoToPlaylist(String playlistName, String videoId) {
-    System.out.println("addVideoToPlaylist needs implementation");
+    for (VideoPlaylist playlist : playlists) {
+      if (playlistName.equalsIgnoreCase(playlist.getName())) {
+        try {
+          Video video = videoLibrary.getVideo(videoId);
+          String title = video.getTitle();
+
+          if (playlist.getVideo(videoId) == null)
+          {
+            playlist.addVideo(video);
+            System.out.printf("Added video to %s: %s\n", playlistName, title);
+            return;
+          }
+          else
+          {
+            System.out.printf("Cannot add video to %s: Video already added\n", playlistName);
+            return;
+          }
+        }
+        catch (NullPointerException e) {
+          System.out.printf("Cannot add video to %s: Video does not exist\n", playlistName);
+          return;
+        }
+      }
+    }
+
+    System.out.printf("Cannot add video to %s: Playlist does not exist\n", playlistName);
   }
 
   public void showAllPlaylists() {
-    System.out.println("showAllPlaylists needs implementation");
+    if (playlists.size() != 0)
+    {
+      System.out.println("Showing all playlists: ");
+      List<String> playlistStrings = new ArrayList<>();
+      for (VideoPlaylist playlist : playlists) {
+        playlistStrings.add(playlist.getName());
+      }
+      Collections.sort(playlistStrings);
+
+      for (String playlist : playlistStrings)
+      {
+        System.out.println(playlist);
+      }
+    }
+    else
+    {
+      System.out.println("No playlists exist yet");
+    }
   }
 
   public void showPlaylist(String playlistName) {
-    System.out.println("showPlaylist needs implementation");
+    for (VideoPlaylist playlist : playlists) {
+      if (playlistName.equalsIgnoreCase(playlist.getName())) {
+        System.out.printf("Showing playlist: %s\n", playlistName);
+        List<Video> videos = playlist.getVideos();
+        if (videos.size() != 0) {
+          for (Video video : videos) {
+            System.out.printf("\t%s\n", video.toString());
+          }
+        }
+        else
+        {
+          System.out.printf("\tNo videos here yet\n");
+        }
+        return;
+      }
+    }
+
+    System.out.printf("Cannot show playlist %s: Playlist does not exist\n", playlistName);
   }
 
   public void removeFromPlaylist(String playlistName, String videoId) {
-    System.out.println("removeFromPlaylist needs implementation");
+    for (VideoPlaylist playlist : playlists) {
+      if (playlistName.equalsIgnoreCase(playlist.getName())) {
+        try {
+          Video video = videoLibrary.getVideo(videoId);
+          String title = video.getTitle();
+
+          if (playlist.getVideo(videoId) != null)
+          {
+            playlist.removeVideo(video);
+            System.out.printf("Removed video from %s: %s\n", playlistName, title);
+            return;
+          }
+          else
+          {
+            System.out.printf("Cannot remove video from %s: Video is not in playlist\n", playlistName);
+            return;
+          }
+        }
+        catch (NullPointerException e) {
+          System.out.printf("Cannot remove video from %s: Video does not exist\n", playlistName);
+          return;
+        }
+      }
+    }
+
+    System.out.printf("Cannot remove video from %s: Playlist does not exist\n", playlistName);
   }
 
   public void clearPlaylist(String playlistName) {
-    System.out.println("clearPlaylist needs implementation");
+    for (VideoPlaylist playlist : playlists) {
+      if (playlistName.equalsIgnoreCase(playlist.getName())) {
+        System.out.printf("Successfully removed all videos from %s\n", playlistName);
+        List<Video> videos = playlist.getVideos();
+        for (Video video : videos) {
+          playlist.removeVideo(video);
+        }
+        return;
+      }
+    }
+
+    System.out.printf("Cannot clear playlist %s: Playlist does not exist\n", playlistName);
   }
 
   public void deletePlaylist(String playlistName) {
-    System.out.println("deletePlaylist needs implementation");
+    for (int x = 0; x < playlists.size(); x++) {
+      if (playlistName.equalsIgnoreCase(playlists.get(x).getName())) {
+        playlists.remove(x);
+        System.out.printf("Deleted playlist: %s\n", playlistName);
+        return;
+      }
+    }
+
+    System.out.printf("Cannot delete playlist %s: Playlist does not exist\n", playlistName);
   }
 
   public void searchVideos(String searchTerm) {
--- src/main/java/com/google/VideoPlaylist.java
+++ src/main/java/com/google/VideoPlaylist.java
@@ -1,6 +1,39 @@
 package com.google;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.ArrayList;
+
 /** A class used to represent a Playlist */
 class VideoPlaylist {
+
+  private String name;
+
+  private LinkedHashMap<String, Video> videos;
+
+  VideoPlaylist(String name) {
+    this.videos = new LinkedHashMap<>();
+    this.name = name;
+  }
+
+  public void addVideo(Video video) {
+    this.videos.put(video.getVideoId(), video);
+  }
+
+  public void removeVideo(Video video) {
+    this.videos.remove(video.getVideoId());
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  Video getVideo(String videoId) {
+    return this.videos.get(videoId);
+  }
+
+  List<Video> getVideos() {
+    return new ArrayList<>(this.videos.values());
+  }
 
 }
